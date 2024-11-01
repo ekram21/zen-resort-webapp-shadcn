fa4b09b0660f910d295943d0f78be5e2c2ea1273
ekram21 <ekram_lol@hotmail.com>
2024-11-02 02:26:31 +0600

    Update profile-form.tsx

--- src/pages/settings/profile/profile-form.tsx
+++ src/pages/settings/profile/profile-form.tsx
@@ -1,27 +1,19 @@
 import { z } from 'zod'
-import { Link } from 'react-router-dom'
-import { useFieldArray, useForm } from 'react-hook-form'
+
+import {  useForm } from 'react-hook-form'
 import { Button } from '@/components/custom/button'
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
-import { Textarea } from '@/components/ui/textarea'
+
 import { toast } from '@/components/ui/use-toast'
-import { cn } from '@/lib/utils'
+
 import { zodResolver } from '@hookform/resolvers/zod'
 
 const profileFormSchema = z.object({
