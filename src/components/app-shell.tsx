2b0815a61cd9baec95251052bd8e5cdf0483d821
ekram21 <ekram_lol@hotmail.com>
2024-11-03 20:51:12 +0600

    Update app-shell.tsx

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -79,7 +79,6 @@ export default function AppShell() {
                         </div>
                     </header>
                     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-                    <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
                         <Outlet />
                     </div>
                 </SidebarInset>
