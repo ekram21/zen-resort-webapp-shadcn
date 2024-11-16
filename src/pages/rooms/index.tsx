27121addd2c0bc10969e55d37530e7ec79c6a089
ekram21 <ekram_lol@hotmail.com>
2024-11-16 14:16:24 +0600

    updates to table

--- src/pages/rooms/index.tsx
+++ src/pages/rooms/index.tsx
@@ -1,16 +1,29 @@
 
-import { Layout } from '@/components/custom/layout'
+import { Layout } from '@/components/custom/layout';
+
+import { DataTable } from './components/data-table'
+import { columns } from './components/columns'
+import { tasks } from './data/tasks'
 
 export default function Rooms() {
 
 
     return (
         <Layout fixed>
-            <Layout.Body className='sm:overflow-hidden'>
-                <section className='flex h-full gap-6'>
+            <Layout.Body>
+                <div className='mb-2 flex items-center justify-between space-y-2'>
+                    <div>
+                        <h2 className='text-2xl font-bold tracking-tight'>Rooms</h2>
+                        <p className='text-muted-foreground'>
+                        You can configure, edit and manage your rooms from here
+                        </p>
+                    </div>
 
+                </div>
+                    <div className='-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0'>
+                    <DataTable data={tasks} columns={columns} />
+                </div>
 
-                </section>
             </Layout.Body>
         </Layout>
     )
